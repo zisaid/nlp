e799e9cbbf027cc249205320b5d3da1be84d6652
f <f@f.f>
2019-04-09 22:41:00 +0800

    分级等功能

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const tokenizer = require('./lib/tokenizer');
 const utils = require('./lib/utils');
+const grade = require('./lib/grade');
 
 let init = function (dictPath) {
   utils.init(dictPath);
+  grade.init(dictPath);
 };
 
 module.exports = {
   tokenizer: tokenizer,
   init: init,
-  utils: utils
+  utils: utils,
+  grade: grade
 };
